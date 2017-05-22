28ca74c2d046ee82bd0cef35e4c304eed8b00302
Michael Warner <michael@lumino.co>
2017-05-21 22:50:59 -0500

    Lot's of changes...
    Moved the scope of some functions/variables, changed the structure of
    the “if layers” of the coin’s response design, added an
    (almost-working) function to parse coin symbols from a sentence,
    improved the help message, and more…

--- app.js
+++ app.js
@@ -1,55 +1,126 @@
+'use strict';
+
 //import our dependencies
 var RtmClient = require('@slack/client').RtmClient,
     http = require('http'),
     CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS,
     RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 
-//declare some variables
+//declare some variables that help the bot function
 var server_url = process.env.SLACK_BOT_TOKEN || 'http://luminoco.slack.com',
     bot_token = process.env.SLACK_BOT_TOKEN || 'xoxb-186234090695-RoK5KbvTNqnWYXpEsFwqTos5',
     bot_name,
     bot_id;
 
-var cryptocurrencyChannel;
-
-//make a new bot
+//make a new bot object
 var bot = new RtmClient(bot_token);
-//initialize the bot's connection
+//initialize the bot's connection to the server
 bot.connect(server_url);
+
+//let's get the coin data from coinmarketcap.com
+var tickerUpdateInterval = 30,
+    tickerData = [],
+    tickerDataOptions = {
+      "method": "GET",
+      "hostname": "api.coinmarketcap.com",
+      "port": null,
+      "path": "/v1/ticker/",
+      "headers": {
+        "cache-control": "no-cache"
+      }
+    };
+
+//get the data initially and continue to refresh it every 'tickerUpdateInterval' seconds
+getTickerData();
+setInterval(getTickerData, tickerUpdateInterval * 1000);
+
+function getTickerData() {
+  let cmcGET = new Promise(function(resolve, reject) {
+    var request = http.request(tickerDataOptions, function(response) {
+      var chunks = [];
+
+      response.on("data", function(chunk) {
+        chunks.push(chunk);
+      });
+
+      response.on("end", function() {
+        var data = Buffer.concat(chunks);
+        resolve(data.toString());
+      });
+    });
+
+    request.on('error', (err) => {
+      console.log('ERROR CONTACTING THE API! %s', err);
+      reject(Error(err));
+    });
+
+    request.end();
+  });
+
+  cmcGET.then(function(data){
+    if(data && data != 'undefined'){
+      tickerData = data;
+      tickerData = JSON.parse(tickerData);
+      return data;
+    }
+  }).catch(function(err){
+    console.log(err);
+    return 'There was an error! ' + err;
+  });
+}
+
+function selectCoinInfo(coin) {
+  return new Promise(function(resolve, reject){
+    //check tickerData for that coin's symbol
+    for (var ticker in tickerData) {
+      if (tickerData[ticker].symbol && tickerData[ticker].symbol !== "undefined" && typeof tickerData[ticker].symbol === 'string' && tickerData[ticker].symbol.toLowerCase() === coin.toLowerCase()){
+        //if/when that coin's symbol is found, return all of that coin's information and stop searching
+        resolve(tickerData[ticker]);
+        break;
+      }
+    }
+  });
+}
+
+function getCoinList(){
+  return new Promise(function(resolve, reject){
+    ((!tickerData) ? reject("There is no data for me to work with!") : tickerData);
+
+    var coinList = [];
+    for (var ticker in tickerData) {
+      coinList.push(tickerData[ticker].symbol);
+      if ((ticker+1) == tickerData.length) {
+        resolve(coinList);
+      }
+    }
+  });
+}
 
 //the client will emit an RTM.AUTHENTICATED event on successful connection
 bot.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function(rtmStartData) {
   bot_name = rtmStartData.self.name;
   bot_id = rtmStartData.self.id;
-  console.log('Successfully logged in as %s of team %s.', rtmStartData.self.name, rtmStartData.team.name);
+  console.log('Successfully logged in as %s of team %s. Bot online. Bleep bloop.', rtmStartData.self.name, rtmStartData.team.name);
 });
 
 //you need to wait for the client to fully connect before you can send messages,
-//so let's include everything in this callback
+//so let's include everything in this callback's scope
 bot.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function() {
-  var tickerDataOptions = {
-    "method": "GET",
-    "hostname": "api.coinmarketcap.com",
-    "port": null,
-    "path": "/v1/ticker/",
-    "headers": {
-      "cache-control": "no-cache"
-    }
-  };
-
   var interestList = require('./interestlist'),
-      tickerData = [];
+      updateChannel;
 
   function update(updateList, channel) {
     if (tickerData.length !== 0 && updateList.length !== 0) {
-      for (coin in updateList) {
-        theCoin = updateList[coin];
-        selectCoinInfo(theCoin.toLowerCase).then(function(coinData){
-          //send the coin data
-          console.log("Selecting...");
-          var updateMessage;
-          ((coinData && coinData.symbol) ? updateMessage = "The current price of " + coinData.symbol.toUpperCase() + ": " + coinData.price_usd + " (" + coinData.price_usd + ")" : updateMessage = "Uh oh! Something went wrong with retrieving the data.");
-          bot.sendMessage(tickerData, channel);
+      for (var coin in updateList) {
+        var updateMessage,
+            target = updateList[coin];
+
+        selectCoinInfo(target).then(function(coinInfo){
+          console.log(coinInfo);
+          ((coinInfo && coinInfo.symbol) ? updateMessage = "The current price of " + coinInfo.symbol.toUpperCase() + " is " + coinInfo.price_btc + " BTC ($" + coinInfo.price_usd + ")" : updateMessage = "Uh oh! Something went wrong with retrieving the data.");
+          bot.sendMessage(updateMessage, channel);
+        }).catch(function(err){
+          ((channel) ? bot.sendMessage(err, channel) : console.log(err));
         });
       }
     } else {
@@ -58,110 +129,145 @@ bot.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function() {
     }
   }
 
-  function selectCoinInfo(coin) {
-    console.log(tickerData);
-    return new Promise(function(resolve, reject){
-      //check tickerData for that coin's symbol
-      for (var index in tickerData) {
-        if (tickerData[index].symbol && tickerData[index].symbol !== "undefined" && typeof tickerData[index].symbol === 'string' && tickerData[index].symbol.toLowerCase() === coin.toLowerCase){
-          //if/when that coin's symbol is found, return all of that coin's information and stop searching
-          resolve(tickerData[index]);
-          break;
-        }
-      }
-    });
-  }
-
-  function getTickerData() {
-    let cmcGET = new Promise(function(resolve, reject) {
-      var request = http.request(tickerDataOptions, function(response) {
-        var chunks = [];
-
-        response.on("data", function(chunk) {
-          chunks.push(chunk);
-        });
-
-        response.on("end", function() {
-          var data = Buffer.concat(chunks);
-          resolve(data.toString());
-        });
-      });
-
-      request.on('error', (err) => {
-        console.log('ERROR CONTACTING THE API! %s', err);
-        reject(Error(err));
-      });
-
-      request.end();
-    });
-
-    cmcGET.then(function(data){
-      if(data && data != 'undefined'){
-        tickerData = data;
-        return data;
-      }
-    }).catch(function(err){
-      console.log(err);
-      return 'There was an error! ' + err;
-    });
-  }
-
-  getTickerData();
-
-  var tickerUpdateInterval = 30,
-      slackUpdateInterval = 6 * 60 * 60;
-
-  setInterval(getTickerData, tickerUpdateInterval * 1000);
-
   //////////// AUTOMATIC INTERACTION ///////////////
-  setInterval(function(){
-    update(interestList, cryptocurrencyChannel);
-  }, slackUpdateInterval * 1000);
+  var slackUpdateInterval,
+      automaticUpdates = setInterval(function(){
+        ((interestList && updateChannel) ? update(interestList, updateChannel) : interestList);
+      }, slackUpdateInterval * 1000);
+
+  setUpdateInterval(6); //default update interval is 6 hours
+
+  bot.on(RTM_EVENTS.TEAM_JOIN, function(user){
+    bot.sendMessage("Welcome to the team! My name is " + bot_name + " and I'm here to keep you updated on cryptocurrency prices. Allow me to send you a list of my commands.");
+    displayHelp(user.id);
+  });
+
+  function setUpdateInterval(interval) {
+    return new Promise(function(resolve, reject){
+      if (typeof interval === 'number') {
+        slackUpdateInterval = interval * 60 * 60;
+        resolve();
+      } else {
+        reject();
+      }
+    });
+  }
 
   //////////// MANUAL INTERACTION ///////////////
   bot.on(RTM_EVENTS.MESSAGE, function(message){
     var text = message.text,
+        author = message.user,
         channel = message.channel;
 
-    var noUnderstand = "I'm sorry, I didn't understand that. Type *cryptobot help* to see a detail of my commands.";
+    var noUnderstand = "I'm sorry, I didn't understand what you asked " + author + ". Type *cryptobot help* to see a detail of my commands.";
 
-    if (text && (text.includes(bot_name) || text.includes(bot_id))){
-      //update(coins);
-      if (text.includes("interest")) {
-        if (text.includes("add")) {
-          saySuccessMessage();
-          //add the interest to interest list
-          //var coin = search the to see if the message includes a symbol or name from tickerData
-          //addInterest(coin, channel);
+    function notValidYet(channel){
+      bot.sendMessage("That functionality hasn't been added yet, but I understand what you're saying. Let's remember to walk before we run, dude. Bleep bloop.", channel);
+    }
 
-          //may need to make this a promise***
-        } else if (text.includes("remove") || text.includes("delete")) {
-          saySuccessMessage();
-          //remove the interest from interest list
-          //var coin = search the to see if the message includes a symbol or name from tickerData
-          //removeInterest(coin, channel);
-
-          //may need to make this a promise***
-        } else if (text.includes("display") || text.includes("show")) {
-          //display the current interest list
-          displayInterest(channel);
+    if (text && (text.includes("!" + bot_name) || text.includes(bot_id)) && author !== bot_name) {
+      //CREATE
+      if (text.includes("add")) {
+        if (text.includes("interest")) {
+          //parse text for all coin references
+          parseCoins(text).then(function(parsedCoins){
+            for (var coin in parsedCoins) {
+              //add them to the list
+              interestList.push(parsedCoins[coin]);
+              if ((coin + 1) == parsedCoins.length) {
+                //update the user on the last cycle of the loop
+                saySuccessMessage("I added " + parsedCoins.join(", ") + " to the interest list.");
+              }
+            }
+          }).catch(function(err){
+            bot.sendMessage(err, channel);
+          });
         } else {
           bot.sendMessage(noUnderstand, channel);
         }
+      //READ
+      } else if (text.includes("display") || text.includes("show") || text.includes("what")) {
+        if (text.includes("price") || text.includes("prices")) {
+         //parse text for all coin references
+         //search to see if it's a valid coin
+         //update them on the price
+         update(interestList, channel);
+        } else if (text.includes("interest")) {
+          bot.sendMessage("The current coins of interest are: " + interestList.join(", ") + ".", channel);
+        } else if (text.includes("update")) {
+          if (text.includes("interval")) {
+            bot.sendMessage("The current automatic update interval is set to " + (slackUpdateInterval / 60 / 60) + " hours.", channel);
+          } else if (text.includes("channel")) {
+            bot.sendMessage("The current channel receiving the automatic updates is " + updateChannel + ".", channel);
+          } else {
+            bot.sendMessage(noUnderstand, channel);
+          }
+        } else {
+          bot.sendMessage(noUnderstand, channel);
+        }
+      //UPDATE
+      } else if (text.includes("update") || text.includes("set")) {
+        if (text.includes("interval")) {
+          var filterInt = function(value) {
+            if (/\d+/.test(value))
+              return Number(value);
+            return NaN;
+          }
+
+          setUpdateInterval(filterInt(text));
+          saySuccessMessage(channel, "You'll be automagically updated on your coins interests every " + filterInt(text) + " hours from now on.");
+        } else if (text.includes("channel")) {
+          setUpdateChannel(channel);
+          bot.sendMessage("This channel will be the channel that gets automatic interest updates every " + slackUpdateInterval + " hours.", channel);
+        } else {
+          bot.sendMessage(noUnderstand, channel);
+        }
+      //DELETE
+      } else if (text.includes("remove") || text.includes("delete")) {
+        if (text.includes("coin") || text.includes("crypto")) {
+          //parse text for all coin references
+          //make sure it's actually on the list
+          //remove it from the list
+
+          //parsedCoins = [];
+          //index = interestList.indexOf(parsedCoins);
+          notValidYet(channel);
+        }
+      //HELP
       } else if (text.includes("help")) {
         //display the list of functions
         displayHelp(channel);
-      } else if (text.includes("update")) {
-        update(interestList, channel);
       } else {
         bot.sendMessage(noUnderstand, channel);
       }
     }
 
-    function saySuccessMessage(channel) {
+    function parseCoins(message) {
+      return new Promise(function(resolve, reject){
+        getCoinList().then(function(coinList){
+          var words = message.split(" "),
+              coinsMentioned = [];
+          //console.log(coinList);
+          for (var word in words) {
+            if (coinList.indexOf(words[word].toUpperCase) > -1) {
+              coinsMentioned.push(words[word]);
+              if ((word + 1) == words.length) {
+                console.log(coinsMentioned);
+                resolve(coinsMentioned);
+              }
+            }
+          }
+        }).catch(function(err){
+          reject(err);
+        });
+      });
+    }
+
+    function saySuccessMessage(channel, addition) {
       var successMessageList = require('./successmessages'),
-          rand = Math.floor((Math.random() * successMessageList.length) + 1)
+          rand = Math.floor((Math.random() * successMessageList.length)),
           successMessage = successMessageList[rand];
+      ((addition) ? successMessage = successMessage + " " + addition : successMessage);
       bot.sendMessage(successMessage, channel);
     }
 
@@ -169,20 +275,14 @@ bot.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function() {
       string = string.toUpperCase();
       if (! interestList.includes(string)) {
         interestList.push(string);
-        saySuccessMessage(channel);
-      } else {
-        bot.sendMessage("That coin is already a part of the interest list.", channel);
       }
     }
 
     function removeInterest(string, channel) {
       string = string.toUpperCase();
-      if (! interestList.includes(string)) {
-        bot.sendMessage("That coin isn't on the interest list.", channel);
-      } else {
+      if (interestList.includes(string)) {
         var index = array.indexOf(string);
         ((index > -1) ? interestList.splice(index, 1) : index);
-        saySuccessMessage(channel);
       }
     }
 
@@ -195,8 +295,14 @@ bot.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function() {
       }
     }
 
+    function setUpdateChannel(channel){
+      updateChannel = channel;
+      saySuccessMessage(channel);
+    }
+
+
     function displayHelp(channel) {
-      bot.sendMessage("You can ask me the following things:\n *display* interest list \n *add* (coin) to interest list \n *remove* (coin) from interest list", channel);
+      bot.sendMessage("You can ask me the following things:\n *add* (coin) to interest list \n *display/show* interest list, update interval, update channel, (coin) price \n *update/set* (time) update interval, (channel) update channel \n *remove/delete* (coin) from interest list \n *help* show this information panel", channel);
     }
   });
 });
